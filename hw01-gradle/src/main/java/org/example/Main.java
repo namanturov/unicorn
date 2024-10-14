bf62fe19f117ff65bbb8c26a2eac395c69606781
Amanturov Nazar <amanturovnazarbek4580@mail.ru>
2024-10-14 21:43:39 +0600

    сделал домашнее задание по первому уроку.

---
+++ hw01-gradle/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        go();
+    }
+
+    private static void go() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<List<Integer>> partitions = Lists.partition(numbers, 3);
+
+        System.out.println("Результат: " + partitions);
+    }
+}
